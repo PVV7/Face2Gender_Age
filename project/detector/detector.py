7fb191623f71e332aa567d8b3fe113bc6187a890
Plotnikov <plotnikovveniamin@gmail.com>
2024-01-23 20:32:03 +0500

    croped image

--- project/detector/detector.py
+++ project/detector/detector.py
@@ -15,23 +15,44 @@ class OnnxModel(object):
         pass
 
     def get_boxes(self):
-        boxes = self._predict[0].boxes.cpu().numpy()
+        boxes = self._predict[0].boxes
+        if boxes is None:
+            return []
+
+        boxes = boxes.cpu().numpy()
         xyxys = boxes.xyxy
 
-        return xyxys.tolist()[0]
+        return xyxys.tolist()
 
-    def view(self):
+    def crop_obj(self):
+        boxes = self.get_boxes()
+        if len(boxes) == 0:
+            return f'лиц нет'
+
+        image = cv2.imread(self._image)
+        for i, box in enumerate(boxes):
+            x1, y1, x2, y2 = box
+            crop_obj = image[int(y1):int(y2), int(x1):int(x2)]
+            cv2.imwrite('croped_image_' + str(i) + '.jpg', crop_obj)
+
+
+    def view_model(self): # временный метод, показывает результаты работы модели
+        return self._predict
+
+
+    def view(self): #временный метод для вывода результата на изображения
         image = self._image
         image = cv2.imread(image)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         xyxys = self.get_boxes()
 
-        image = cv2.rectangle(image,
-                              (int(xyxys[0]), int(xyxys[1])),
-                              (int(xyxys[2]), int(xyxys[3])),
-                              (255, 0, 0),
-                              2)
+        for xyxy in xyxys:
+            image = cv2.rectangle(image,
+                                  (int(xyxy[0]), int(xyxy[1])),
+                                  (int(xyxy[2]), int(xyxy[3])),
+                                  (255, 0, 0),
+                                  2)
 
         plt.imshow(image)
         plt.show()
